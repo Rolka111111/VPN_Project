e799c4ef9caa1de71210f78989368e650cded148
owl64 <hermawandi64@gmail.com>
2023-05-06 21:26:05 +0700

    modified:   install-xray.sh

--- install-xray.sh
+++ install-xray.sh
@@ -398,6 +398,10 @@ cat >/etc/nginx/conf.d/xray.conf <<EOF
     server {
              listen 80;
              listen [::]:80;
+             listen 8080;
+             listen [::]:8080;
+             listen 8880;
+             listen [::]:8880;
              listen 443 ssl http2 reuseport;
              listen [::]:443 http2 reuseport;	
              server_name $domain;
