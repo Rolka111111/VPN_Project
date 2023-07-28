d64a416b941b1fbbf7f45122b2b3417f70670bca
owl64 <hermawandi64@gmail.com>
2023-07-29 00:51:04 +0700

    modified:   updated/delldumy.sh

--- updated/delldumy.sh
+++ updated/delldumy.sh
@@ -1,16 +1,5 @@
-#!/bin/bash
-# ==========================================
-# Color
-RED='\033[0;31m'
-NC='\033[0m'
-GREEN='\033[0;32m'
-ORANGE='\033[0;33m'
-BLUE='\033[0;34m'
-PURPLE='\033[0;35m'
-CYAN='\033[0;36m'
-LIGHT='\033[0;37m'
-# ==========================================
-read -rp "Username : " -e userdell
+read -rp "Username : " -e user
+read -rp "Expired(days) : " -e exp
 sed -i "/^### $user $exp/,/^},{/d" /etc/xray/config.json
 sed -i "/^### $user $exp/,/^},{/d" /etc/xray/config.json
 rm -f /etc/xray/vmess-$user-tls.json /etc/xray/vmess-$user-nontls.json /etc/xray/vmess-$user-grpc.json
