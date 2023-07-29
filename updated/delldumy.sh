b974fea29bc72ecf643b4c0b60fdc9cba5b527b6
owl64 <hermawandi64@gmail.com>
2023-07-29 17:30:55 +0700

    deleted:    panel/add.php 	new file:   panel/pdellvmess.sh 	deleted:    updated/delldumy.sh

--- updated/delldumy.sh
+++
@@ -1,7 +0,0 @@
-read -rp "Username : " -e user
-read -rp "Expired(days) : " -e exp
-sed -i "/^### $user $exp/,/^},{/d" /etc/xray/config.json
-sed -i "/^### $user $exp/,/^},{/d" /etc/xray/config.json
-rm -f /etc/xray/vmess-$user-tls.json /etc/xray/vmess-$user-nontls.json /etc/xray/vmess-$user-grpc.json
-systemctl restart xray.service
-
