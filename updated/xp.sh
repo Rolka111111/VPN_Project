c0aa4ac2530133a03147e5357277313e083ad092
owl64 <hermawandi64@gmail.com>
2023-09-07 12:35:36 +0700

    modified:   updated/xp.sh

--- updated/xp.sh
+++ updated/xp.sh
@@ -11,17 +11,17 @@ CYAN='\033[0;36m'
 LIGHT='\033[0;37m'
 # ==========================================
 # Getting
-MYIP=$(wget -qO- ipinfo.io/ip);
-echo "Checking VPS"
-IZIN=$( curl https://raw.githubusercontent.com/AkbarStoreVPN/perizinan/main/ipvps.txt | grep $MYIP )
-if [ $MYIP = $MYIP ]; then
-echo -e "${NC}${GREEN}Permission Accepted...${NC}"
-else
-echo -e "${NC}${RED}Permission Denied!${NC}";
-echo -e "${NC}${LIGHT}Please Contact Admin!!"
-echo -e "${NC}${LIGHT}Telegram : https://t.me/lizadm"
-exit 0
-fi
+# MYIP=$(wget -qO- ipinfo.io/ip);
+# echo "Checking VPS"
+# IZIN=$( curl https://raw.githubusercontent.com/AkbarStoreVPN/perizinan/main/ipvps.txt | grep $MYIP )
+# if [ $MYIP = $MYIP ]; then
+# echo -e "${NC}${GREEN}Permission Accepted...${NC}"
+# else
+# echo -e "${NC}${RED}Permission Denied!${NC}";
+# echo -e "${NC}${LIGHT}Please Contact Admin!!"
+# echo -e "${NC}${LIGHT}Telegram : https://t.me/lizadm"
+# exit 0
+# fi
 
 # CODE AKUN XRAY DI /etc/xray/config.json
 # ====================
