502b3699d195b89b01496124dd5820b2be2a4935
owl64 <hermawandi64@gmail.com>
2023-11-17 14:27:45 +0700

    modified:   updated/rennewvmess.sh

--- updated/rennewvmess.sh
+++ updated/rennewvmess.sh
@@ -10,6 +10,18 @@ PURPLE='\033[0;35m'
 CYAN='\033[0;36m'
 LIGHT='\033[0;37m'
 # ==========================================
+function con() {
+    local -i bytes=$1;
+    if [[ $bytes -lt 1024 ]]; then
+        echo "${bytes}B"
+    elif [[ $bytes -lt 1048576 ]]; then
+        echo "$(( (bytes + 1023)/1024 ))KB"
+    elif [[ $bytes -lt 1073741824 ]]; then
+        echo "$(( (bytes + 1048575)/1048576 ))MB"
+    else
+        echo "$(( (bytes + 1073741823)/1073741824 ))GB"
+    fi
+}
 # Getting
 MYIP=$(wget -qO- ipinfo.io/ip);
 echo "Checking VPS"
@@ -47,6 +59,8 @@ NUMBER_OF_CLIENTS=$(grep -E "^#vms# " "/etc/xray/config.json" | sort | uniq | cu
 
 user=$(grep -E "^#vms# " "/etc/xray/config.json" | sort | uniq | cut -d ' ' -f 2 | sed -n "${CLIENT_NUMBER}"p)
 exp=$(grep -E "^#vms# " "/etc/xray/config.json" | sort | uniq | cut -d ' ' -f 3 | sed -n "${CLIENT_NUMBER}"p)
+byte=$(cat /etc/vmess/${user})
+lim=$(con ${byte})
 
 clear
 echo -e ""
@@ -55,8 +69,10 @@ echo -e "      Account Information"
 echo -e "==============================="
 echo -e "${CLAY}Username : $user "
 echo -e "Expired  : $exp ${NC}"
+read -p "Limit User (GB): $lim"
 echo -e "==============================="
 read -p "Add Expired (Days): " masaaktif
+read -p "Limit User (GB): " Quota
 echo -e "Please Wait...."
 sleep 3
 now=$(date +%Y-%m-%d)
@@ -65,8 +81,25 @@ d2=$(date -d "$now" +%s)
 exp2=$(( (d1 - d2) / 86400 ))
 exp3=$(($exp2 + $masaaktif))
 exp4=`date -d "$exp3 days" +"%Y-%m-%d"`
-sed -i "s/#vms# $user $exp/### $user $exp4/g" /etc/xray/config.json
-sed -i "s/#vms# $user $exp/### $user $exp4/g" /etc/xray/config.json
+sed -i "s/#vms# $user $exp/#vms# $user $exp4/g" /etc/xray/config.json
+sed -i "s/#vms# $user $exp/#vms# $user $exp4/g" /etc/xray/config.json
+
+if [ -z ${Quota} ]; then
+  Quota="0"
+fi
+
+c=$(echo "${Quota}" | sed 's/[^0-9]*//g')
+d=$((${c} * 1024 * 1024 * 1024))
+e=$((${byte} + ${d}))
+
+if [[ ${c} > "0" ]]; then
+  echo "${e}" >/etc/vmess/${user}
+fi
+DATADB=$(cat /etc/vmess/.vmess.db | grep "^#vms#" | grep -w "${user}" | awk '{print $2}')
+if [[ "${DATADB}" != '' ]]; then
+  sed -i "/\b${user}\b/d" /etc/vmess/.vmess.db
+fi
+echo "#vms# ${user} ${exp4} ${uuid} ${Quota}" >>/etc/vmess/.vmess.db
 systemctl restart xray.service
 service cron restart
 clear
@@ -76,5 +109,6 @@ echo "  XRAYS/Vmess Account Renewed  "
 echo "==============================="
 echo "Username  : $user"
 echo "Expired   : $exp4"
+echo "Limit User (GB): $lim"
 echo "==============================="
 echo "Script Mod By andi64"
